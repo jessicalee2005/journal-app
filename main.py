217d2c57e7a13db9d241ddb1d89ce9e2b0c8b800
Huy Nguyen <152629349+OuiHui@users.noreply.github.com>
2025-02-22 09:43:36 -0500

    add python files

---
+++ main.py
@@ -0,0 +1,21 @@
+import pyaudio
+import websockets
+import asyncio
+import base64
+import json
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+
+p = pyaudio.PyAudio
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_BUFFER
+)
+
